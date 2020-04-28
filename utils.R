416dcc529db8dc8a8967548aeb54bdda35846655
Valeri Voev <valeri.radkov.voev@lego.com>
2020-04-28 14:24:17 +0200

    tables scraping

--- utils.R
+++ utils.R
@@ -1,7 +1,7 @@
 get_hosp_table <- function(web_url){
 	
 	webpage <- xml2::read_html(webpage_url)
-	tables <- rvest::html_table(webpage, header = TRUE)
+	tables <- rvest::html_table(webpage, header = TRUE, fill = TRUE)
 	headers <- sapply(tables, function(x) x[1,1])	
 	hosp_index = min(which(headers == "Stigning i forhold til dagen før"))
 	hosp <- tables[[hosp_index]]
